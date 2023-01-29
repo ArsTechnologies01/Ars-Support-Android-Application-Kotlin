a6443b82c200c45857f0a12a5ff6d8e14a1a7166
Joy Mridha <joymridha2004@gmail.com>
2023-01-29 11:42:14 +0530

    Signup Condition Added

--- app/src/main/java/com/example/arstrack/activity/SignUpActivity.kt
+++ app/src/main/java/com/example/arstrack/activity/SignUpActivity.kt
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
 import android.os.Bundle
 import android.content.Intent
+import android.util.Patterns
 import android.view.View
 import com.example.arstrack.R
+import com.google.android.material.textfield.TextInputLayout
+import java.util.*
 
 
 class SignUpActivity : AppCompatActivity() {
@@ -40,8 +43,78 @@ class SignUpActivity : AppCompatActivity() {
 
         findViewById<View>(R.id.signUpButton).setOnClickListener(
             View.OnClickListener {
+
+                if (!validateName() or !validateEmail() or !validatePhoneNumber() or !validatePassword() or !validateConfirmPassword()) {
+                    return@OnClickListener
+                }
+                if (Objects.requireNonNull(password?.getText())
+                        .toString() != Objects.requireNonNull(
+                        confirmPassword?.getText()
+                    ).toString()
+                ) {
+                    val passwordLayout =
+                        findViewById<TextInputLayout>(R.id.confirmPasswordEditTextLayout)
+                    passwordLayout.error = "Password does not match"
+                    return@OnClickListener
+                }
                 startActivity(Intent(this, MainActivity::class.java))
             })
+    }
 
+    /* ------------SignUp Details validation----------------*/
+    private fun validateName(): Boolean {
+        val `val` = Objects.requireNonNull(fullName!!.text).toString()
+        if (`val`.isEmpty()) {
+            fullName!!.error = "Field cannot be empty!"
+            return false
+        }
+        return true
+    }
+
+    private fun validateEmail(): Boolean {
+        val `val` = Objects.requireNonNull(email!!.text).toString()
+        if (`val`.isEmpty()) {
+            email!!.error = "Cannot be empty!"
+            return false
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(`val`).matches()) {
+            email!!.error = "Enter valid email"
+            return false
+        }
+        return true
+    }
+
+    private fun validatePhoneNumber(): Boolean {
+        val `val` = Objects.requireNonNull(phoneNumber!!.text).toString()
+        if (`val`.isEmpty()) {
+            phoneNumber!!.error = "Cannot be empty"
+            return false
+        }
+        return true
+    }
+
+    private fun validatePassword(): Boolean {
+        val passwordLayout = findViewById<TextInputLayout>(R.id.passwordEditTextLayout)
+        val `val` = Objects.requireNonNull(passwordLayout.editText)?.text.toString()
+        val passwordMatcher = "^" + "(?=.*[@#$%^&+=])" +  // at least 1 special character
+                "(?=\\S+$)" +  // no white spaces
+                ".{4,}" +  // at least 4 characters
+                "$"
+        if (`val`.isEmpty()) {
+            passwordLayout.error = "Cannot be empty!"
+            return false
+        }
+        passwordLayout.isErrorEnabled = false
+        return true
+    }
+
+    private fun validateConfirmPassword(): Boolean {
+        val passwordLayout = findViewById<TextInputLayout>(R.id.confirmPasswordEditTextLayout)
+        val `val` = Objects.requireNonNull(passwordLayout.editText)?.text.toString()
+        if (`val`.isEmpty()) {
+            passwordLayout.error = "Cannot be empty!"
+            return false
+        }
+        passwordLayout.isErrorEnabled = false
+        return true
     }
 }
